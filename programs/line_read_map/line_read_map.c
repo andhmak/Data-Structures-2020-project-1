514794512444514c80c4acd90d44664568dad8b9
andhmak <sdi1900047@di.uoa.gr>
2020-03-14 01:19:34 +0200

    first implementation

---
+++ programs/line_read_map/line_read_map.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ADTMap.h"
+#define LINELENGTH 200
+
+char* create_string(char* string) {
+    char* newstring;
+    newstring = malloc(sizeof(char) * (strlen(string) + 1));
+    strcpy(newstring, string);
+    return newstring;
+}
+
+int* create_int(int a) {
+    int* p = malloc(sizeof(int));
+	*p = a;
+	return p;
+}
+
+int main(void) {
+    Map map = map_create(strcmp, free, free);
+    char linebuf[LINELENGTH];
+    int* times;
+    MapNode node;
+    while (fgets(linebuf, LINELENGTH, stdin) != NULL) {
+        if ((times = (int*)map_find(map, linebuf)) == NULL) {
+            printf("0\n");
+            map_insert(map, create_string(linebuf), create_int(1));
+        }
+        else {
+            printf("%d\n", *times);
+            map_insert(map, create_string(linebuf), create_int(*times + 1));
+        }
+    }
+    return 0;
+}
