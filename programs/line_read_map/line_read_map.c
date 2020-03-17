b7ab417f4925b1e7717d9b5feab1243b46f406eb
andhmak <sdi1900047@di.uoa.gr>
2020-03-17 10:54:11 +0200

    Relacing create_string with strdup

--- programs/line_read_map/line_read_map.c
+++ programs/line_read_map/line_read_map.c
@@ -4,13 +4,6 @@
 #include "ADTMap.h"
 #define LINELENGTH 200
 
-char* create_string(char* string) {
-    char* newstring;
-    newstring = malloc(sizeof(char) * (strlen(string) + 1));
-    strcpy(newstring, string);
-    return newstring;
-}
-
 int* create_int(int a) {
     int* p = malloc(sizeof(int));
 	*p = a;
@@ -24,11 +17,11 @@ int main(void) {
     while (fgets(linebuf, LINELENGTH, stdin) != NULL) {
         if ((times = (int*)map_find(map, linebuf)) == NULL) {
             printf("0\n");
-            map_insert(map, create_string(linebuf), create_int(1));
+            map_insert(map, strdup(linebuf), create_int(1));
         }
         else {
             printf("%d\n", *times);
-            map_insert(map, create_string(linebuf), create_int(*times + 1));
+            map_insert(map, strdup(linebuf), create_int(*times + 1));
         }
     }
     map_destroy(map);
